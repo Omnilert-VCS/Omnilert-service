341a2451d3585c1ec31cedef030edfb3ce85fc90
lightyagami1427 <117759974+lightyagami1427@users.noreply.github.com>
2025-11-03 11:16:00 +0530

    Update sc.py

--- sc.py
+++ sc.py
@@ -11,7 +11,7 @@ ghp_abc123def456ghi789jkl012mno345pqr679
 
 test@gmail.com
 akhilnishtala14@gmail.com
-aio_abcdefghijklmnopqrstuvwxyz01 
+aio_abcdefghijklmnopqrstuvwxyz0 
 # +
 ghp_abc123def456ghi789jkl012mno345pqr678 
 GOOA1234567890123456 
